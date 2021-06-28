cfa6b8d4c32bdbe58e147fca79d8a859918e858a
Bay Bui <baybui@ucla.edu>
2021-06-27 17:12:50 -0700

    heroku

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Budget Tracker Starter Code
+# Budget Tracker
+
+## Description
+
+An app that can be used to track you daily budget.
+
+![Budget Tracker](budget-tracker_screengrab_.png)
+
+## Tables of Contents
+
+- [Installation](#installation)
+- [Deployed App](#deployed-app)
+- [Usage](#usage)
+- [Questions](#questions)
+
+## Installation
+
+- npm init --y
+- npm install
+
+## Deployed App
+
+## Usage
+
+An app that can be used to track you daily budget that can be used offline and downloaded as a PWA on to your mobile device via your web browser
+
+## Contributing
+
+N/A
+
+## Questions
+
+For any questions please reach out to
+github: [Unity7](https://github.com/unity7)
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
@@ -16,11 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-// app.use("/icons", express.static(path.join(__dirname, "public/icons")));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker', {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
